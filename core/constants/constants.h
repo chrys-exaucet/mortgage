f3e34e851772b0f59437faf3e9be33a7f3ebf841
chrys-exaucet <chrysexaucet@hotmail.fr>
2021-02-11 19:37:55 +0000

    fixed consts definition (#1)

--- core/constants/constants.h
+++ core/constants/constants.h
@@ -6,24 +6,23 @@
 #define MORTGAGE_CONSTANTS_H
 
 // IBAN related
-
-#define DEFAULT_IBAN_START "FR21"
-#define DEFAULT_BANK_CODE 30001
-#define DEFAULT_AGENCY_CODE 00021
-#define DEFAULT_ACCOUNT_NUMBER 0000000001
-#define DEFAULT_ACCOUNT_NUMBER_INCREMENT 2
-#define DEFAULT_KEY 10
-#define DEFAULT_KEY_INCREMENT 2
+#define DEFAULT_IBAN_START  "FR21"
+#define DEFAULT_BANK_CODE  30001
+#define DEFAULT_AGENCY_CODE  00021
+#define DEFAULT_ACCOUNT_NUMBER  0000000001
+#define DEFAULT_ACCOUNT_NUMBER_INCREMENT  2
+#define DEFAULT_KEY  10
+#define DEFAULT_KEY_INCREMENT  2
 
 
 // Bank related
-static const float FIXED_BANK_RATE = 0.0095;
-static const float CAPED_BANK_RATE_LIMIT = 0.10;
-static const unsigned long DEFAULT_NOTARY_FEE = 15000;
+#define FIXED_BANK_RATE  0.0095
+#define CAPED_BANK_RATE_LIMIT  0.10
+#define DEFAULT_NOTARY_FEE  15000
 
 
-// health care percentage
-static const double GOOD_HEALTH_CARE_TAX = 0.0090;
-static const double BAD_HEALTH_CARE_TAX = 0.03;
+// health care percentage tax
+#define GOOD_HEALTH_CARE_TAX  0.0090
+#define  BAD_HEALTH_CARE_TAX  0.03
 
 #endif //MORTGAGE_CONSTANTS_H
